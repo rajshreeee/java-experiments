d50f6c0f67ca8651d7bf0e4217ec6f4a0997e07f
silwalanish <silwalanish@gmail.com>
2019-08-08 15:07:30 +0545

    Composition Challenge Completed

---
+++ anish/Challenges.java
@@ -0,0 +1,12 @@
+package anish;
+
+// import anish.constructors.Challenge;   // Constructors Challenge
+
+import anish.composition.Challenge;    // Composition Challenge
+
+public class Challenges {
+
+    public static void main(String[] args) {
+        Challenge.run();
+    }
+}
